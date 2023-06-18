52d3df7d87eaf3152bd62339cd8aefa50844fd8c
PremShakti <premshakti.ps@gmail.com>
2023-06-18 17:58:53 +0530

    a

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,7 +8,7 @@ console.log(img,title)
   return (
     <Box overflow={"hidden"} boxShadow={'rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px'} borderRadius={"10px"} pb={"10px"}>
         <a href={video}> <Image w={"100%"} src={img} />
-      <Text fontSize={"sm"} fontWeight={"bold"} >{title}</Text></a>
+      <Text fontSize={"sm"} fontWeight={"bold"} w={"95%"} display={'block'} m={"auto"} mt={"8px"} textAlign={"center"} >{title}</Text></a>
      
     </Box>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <>
     <Box display={'flex'} alignItems={'center'} justifyContent={"center"} h={"60px"}bg={"#3c11ff"} >
-     <Text fontWeight={"bold"} color={"white"} fontStyle={"25px"}  >Movie Adda</Text> 
+     <Text fontWeight={"bold"} color={"white"} fontSize={"25px"}  >Movie Adda</Text> 
     </Box>
       
     </>
